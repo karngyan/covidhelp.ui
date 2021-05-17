7a812c45c78b5e51e25b03cfc05c559db6df5c88
Karn <mail@karngyan.com>
2021-05-17 19:56:24 +0530

    add sitemap

--- nuxt.config.js
+++ nuxt.config.js
@@ -55,14 +55,16 @@ export default {
   ],
 
   // Modules: https://go.nuxtjs.dev/config-modules
-  modules: [
+
+    modules: [
     // https://go.nuxtjs.dev/axios
     '@nuxtjs/axios',
     // https://go.nuxtjs.dev/content
     '@nuxt/content',
     '@nuxtjs/firebase',
     ['nuxt-buefy', { css: true }],
-    'nuxt-i18n'
+    'nuxt-i18n',
+    '@nuxtjs/sitemap',
   ],
   i18n: {
     lazy: true,
